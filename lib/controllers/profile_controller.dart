55f16af325d2d8c83850abd9e82dc1564ce8af69
Cyrus Rol <cyrusrol@outlook.com>
2024-11-10 15:27:43 +0800

    Fixed the follow bugs

--- lib/controllers/profile_controller.dart
+++ lib/controllers/profile_controller.dart
@@ -17,7 +17,7 @@ class ProfileController extends GetxController {
   getUserData() async {
     List<String> thumbnails = [];
     var myVideos = await firestore.collection('videos').where('uid', isEqualTo: uid.value).get();
-    for(int i=0; i<myVideos.docs.length; i++){
+    for (int i = 0; i < myVideos.docs.length; i++) {
       thumbnails.add((myVideos.docs[i].data() as dynamic)['thumbnail']);
     }
 
@@ -26,53 +26,59 @@ class ProfileController extends GetxController {
     String name = userData['name'];
     String profilePhoto = userData['profilePhoto'];
     int likes = 0;
-    int followers = 0;
-    int following = 0;
-    bool isFollowing = false;
 
-    for(var item in myVideos.docs){
+    for (var item in myVideos.docs) {
       likes += (item.data()['likes'] as List).length;
     }
+
     var followerDoc = await firestore.collection('users').doc(uid.value).collection('followers').get();
-    var followingDoc = await firestore.collection('users').doc(uid.value).collection('followers').get();
-    followers = followerDoc.docs.length;
-    following = followingDoc.docs.length;
+    var followingDoc = await firestore.collection('users').doc(uid.value).collection('following').get();
+    int followers = followerDoc.docs.length;
+    int following = followingDoc.docs.length;
 
-    firestore.collection('users').doc(uid.value).collection('followers').doc(authController.user.uid).get().then((value) {
-      if(value.exists){
-        isFollowing = true;
-      }else{
-        isFollowing = false;
-      }
-
-    });
+    bool isFollowing = (await firestore.collection('users').doc(uid.value)
+        .collection('followers').doc(authController.user.uid).get()).exists;
 
     user.value = {
-      'followers':followers.toString(),
-      'following':following.toString(),
-      'isFollowing':isFollowing,
-      'likes':likes.toString(),
-      'profilePhoto':profilePhoto,
-      'name':name,
-      'thumbnails':thumbnails
+      'followers': followers.toString(),
+      'following': following.toString(),
+      'isFollowing': isFollowing,
+      'likes': likes.toString(),
+      'profilePhoto': profilePhoto,
+      'name': name,
+      'thumbnails': thumbnails
     };
     update();
   }
 
   followUser() async {
-    var doc = await firestore.collection('users').doc(uid.value).collection('followers').doc(authController.user.uid).get();
-    if(!doc.exists){
-      await firestore.collection('users').doc(uid.value).collection('followers').doc(authController.user.uid).set({});
-      await firestore.collection('users').doc(authController.user.uid).collection('followers').doc(uid.value).set({});
-      user.value.update('follower', (value) => (int.parse(value) + 1).toString());
-  }
-  else{
-    await firestore.collection('users').doc(uid.value).collection('followers').doc(authController.user.uid).delete();
-      await firestore.collection('users').doc(authController.user.uid).collection('followers').doc(uid.value).delete();
-      user.value.update('follower', (value) => (int.parse(value) - 1).toString());
-  }
+    final currentUserId = authController.user.uid;
+    final targetUserId = uid.value;
 
-  user.value.update('isFollowing', (value) => !value);
-  update();
-}
+    var doc = await firestore.collection('users').doc(targetUserId).collection('followers').doc(currentUserId).get();
+
+    if (!doc.exists) {
+      // Add current user to target user's followers
+      await firestore.collection('users').doc(targetUserId).collection('followers').doc(currentUserId).set({});
+
+      // Add target user to current user's following
+      await firestore.collection('users').doc(currentUserId).collection('following').doc(targetUserId).set({});
+
+      // Update follower and following counts correctly
+      user.value.update('followers', (value) => (int.parse(value) + 1).toString());
+    } else {
+      // Remove current user from target user's followers
+      await firestore.collection('users').doc(targetUserId).collection('followers').doc(currentUserId).delete();
+
+      // Remove target user from current user's following
+      await firestore.collection('users').doc(currentUserId).collection('following').doc(targetUserId).delete();
+
+      // Update follower and following counts correctly
+      user.value.update('followers', (value) => (int.parse(value) - 1).toString());
+    }
+
+    // Toggle follow status
+    user.value.update('isFollowing', (value) => !value);
+    update();
+  }
 }

1d4af7221ad8855206f0a44a5ef8abc1b9edbd7c
Cyrus Rol <cyrusrol@outlook.com>
2024-11-19 15:08:09 +0800

    fix search bugs

--- lib/controllers/search_controller.dart
+++ lib/controllers/search_controller.dart
@@ -6,16 +6,25 @@ import 'package:get/get.dart';
 class SearchController extends GetxController {
   final Rx<List<User>> _searchedUsers = Rx<List<User>>([]);
 
-  List<User> get searchedUsers => _searchedUsers.value; 
+  List<User> get searchedUsers => _searchedUsers.value;
 
-  searchUser(String typedUser) async{
-    _searchedUsers.bindStream(
-      firestore.collection('users').where('name', isGreaterThanOrEqualTo: typedUser).snapshots().map((QuerySnapshot query ) {
-        List<User> retVal = [];
-        for(var elem in query.docs) {
-          retVal.add(User.fromSnap(elem));
+  void searchUser(String typedUser) {
+    if (typedUser.isEmpty) {
+      _searchedUsers.value = [];
+      return;
+    }
+
+    String searchKey = typedUser.toLowerCase();
+
+    firestore.collection('users').snapshots().listen((QuerySnapshot query) {
+      List<User> retVal = [];
+      for (var elem in query.docs) {
+        User user = User.fromSnap(elem);
+        if (user.name.toLowerCase().contains(searchKey)) {
+          retVal.add(user);
         }
-        return retVal;
-    }));
+      }
+      _searchedUsers.value = retVal;
+    });
   }
 }
